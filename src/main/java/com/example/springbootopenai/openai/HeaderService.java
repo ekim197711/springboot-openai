95b336d113b5659669fc611d976d948214e6d7a3
Mike Nielsen <mike.nielsen@consultant-podimo.com>
2024-07-21 23:07:31 +0200

    Transcription

---
+++ src/main/java/com/example/springbootopenai/openai/HeaderService.java
@@ -0,0 +1,25 @@
+package com.example.springbootopenai.openai;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HeaderService {
+
+    public HttpHeaders headersTTS() {
+        String openaiApiKey = System.getenv("OPENAI_API_KEY");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization", "Bearer " + openaiApiKey);
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, "application/json");
+        return httpHeaders;
+    }
+
+    public HttpHeaders headersMultipartFormData() {
+        String openaiApiKey = System.getenv("OPENAI_API_KEY");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization", "Bearer " + openaiApiKey);
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA.toString());
+        return httpHeaders;
+    }
+}
